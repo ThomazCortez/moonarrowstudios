8147ab685f416ae4ee36411f52b6681813116daf
Thomaz Cortez Faria Barrago <a30743@aemtg.pt>
2025-05-28 10:24:48 +0100

    index header white box fix

--- index.php
+++ index.php
@@ -68,7 +68,6 @@ function getAssetCount($conn) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>MoonArrow Studios</title>
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.2/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Anonymous+Pro&display=swap" rel="stylesheet">
     <style>
         :root {
